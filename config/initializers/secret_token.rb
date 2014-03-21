d4d93a90ff5965636f2f1ff36242f9e5b0e63e0b
elizahyde <elizabeth@elizahyde.com>
2014-03-21 09:29:46 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mychillpill::Application.config.secret_key_base = '7f2f61bb23b535d480ecaab5d531d0d5e98b8b76f9dca36d3a6f1209d01cd3f210568a2de83d75d756f6a4b542aa2fd26f766e29284ea11fc1b39d973b28c1ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mychillpill::Application.config.session_store :cookie_store, key: '_mychillpill_session'
